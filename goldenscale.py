e4d910bf1c856f4d53e799c71e02b173ff8bb94c
golDenScale <fayfifi.s@gmail.com>
2024-03-27 22:24:38 +0800

    Create goldenscale.py

---
+++ goldenscale.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# 作者：golDen-Scale
+# 版本：1.0
+# License: MIT License
+
+
+import os
+import re
+import io
+import ssl
+import json
+import argparse
+import string
+import logging
+import time
+import requests
+import urllib
+import urllib.parse
+import scrapy
+import beautifulsoup4
+import threading
+import string
+from selenium import webdriver
+
+
+def main():
+
+# 主动扫描
+
+
+
+
+
+
+
+# 被动扫描
+
+
+
+
+
+
+
+# 存储结果
+
+
+
+
+# 提取关键字
+
+
+
+
+
+
+# 对比漏洞库
+
+
+
+
+
+# 输出漏洞利用建议
+
+
+
+
+    if __name__ == "__main__":
+        main()
